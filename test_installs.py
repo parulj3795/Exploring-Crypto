9fa936631d76be980a9a1e39b548a9344518e4b1
parulj3795 <parulj3795@gmail.com>
2024-03-07 21:12:13 +0000

    EDA, CCXT, TA-Lib, Backtrader

--- test_installs.py
+++ test_installs.py
@@ -1,7 +1,9 @@
 import ccxt
 import pandas as pd
 import matplotlib.pyplot as plt
+import talib
 
 # Print versions to verify installation
 print("CCXT Version:", ccxt.__version__)
 print("Pandas Version:", pd.__version__)
+print("TA-lib Version:", talib.__version__)
