027644eada7308cf02aa94f7aab95e5d6e534905
parulj3795 <parulj3795@gmail.com>
2024-03-07 19:46:27 +0000

    Initial commit

---
+++ test_installs.py
@@ -0,0 +1,7 @@
+import ccxt
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Print versions to verify installation
+print("CCXT Version:", ccxt.__version__)
+print("Pandas Version:", pd.__version__)
